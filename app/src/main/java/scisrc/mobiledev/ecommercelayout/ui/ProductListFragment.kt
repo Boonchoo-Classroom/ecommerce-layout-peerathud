be22e5c6ed276ae96076c36483ee4d6b10fec735
poppy <poppypopcorn@gmail.com>
2025-02-22 22:57:07 +0700

    i think complete

--- app/src/main/java/scisrc/mobiledev/ecommercelayout/ui/ProductListFragment.kt
+++ app/src/main/java/scisrc/mobiledev/ecommercelayout/ui/ProductListFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import scisrc.mobiledev.ecommercelayout.R
-
+import android.widget.Button
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -18,43 +18,27 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ProductListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_product_list, container, false)
+
+        val buttonStationery: Button = view.findViewById(R.id.category_stationery)
+        val buttonOutfit:Button =view.findViewById(R.id.category_clothing)
+        val buttonsport:Button =view.findViewById(R.id.category_sports)
+        val buttonheadphone:Button=view.findViewById(R.id.category_gadgets)
+
+        buttonStationery.setOnClickListener { navigateToCategory(StationaryFragment()) }
+        buttonOutfit.setOnClickListener { navigateToCategory(OutfitFragment()) }
+        buttonsport.setOnClickListener { navigateToCategory(SportFragment()) }
+        buttonheadphone.setOnClickListener { navigateToCategory(HeadphoneFragment()) }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProductListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProductListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun navigateToCategory(fragment: Fragment) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
